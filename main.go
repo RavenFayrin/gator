21d293a2576f8455e47706bc7e78208092e7cb33
RavenFayrin <raven.fayrin@gmail.com>
2026-06-22 16:21:34 -0400

    added login in command

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 
 	"main/internal/config"
 )
+
+type state struct {
+	cfg *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("ERROR: Login expects username")
+	}
+	err := s.cfg.SetUser(cmd.args[0])
+	if err != nil {
+		return err
+	}
+	fmt.Printf("username has been set to s%\n", cmd.args[0])
+	return nil
+}
 
 func main() {
 	cfg, err := config.Read()
