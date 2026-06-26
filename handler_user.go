e70d4587cf64c2e7d477c31f6d030f9b1b7d8032
RavenFayrin <raven.fayrin@gmail.com>
2026-06-26 17:41:18 -0400

    one giant mess

--- handler_user.go
+++ handler_user.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+
+	"main/internal/database"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -10,11 +17,56 @@ func handlerLogin(s *state, cmd command) error {
 	}
 	name := cmd.Args[0]
 
-	err := s.cfg.SetUser(name)
+	_, err := s.db.GetUser(context.Background(), name)
+	if err == sql.ErrNoRows {
+		return fmt.Errorf("user does not exist")
+	}
+	if err != nil {
+		return err
+	}
+
+	err = s.cfg.SetUser(name)
 	if err != nil {
 		return fmt.Errorf("couldn't set current user: %w", err)
 	}
 
 	fmt.Println("User switched successfully!")
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <name>", cmd.Name)
+	}
+	name := cmd.Args[0]
+
+	_, err := s.db.GetUser(context.Background(), name)
+	if err == nil {
+		return fmt.Errorf("user already exists")
+	}
+	if err != sql.ErrNoRows {
+		return err
+	}
+
+	user, err := s.db.CreateUser(
+		context.Background(),
+		database.CreateUserParams{
+			ID:        uuid.New(),
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+			Name:      name,
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("couldn't register user: %w", err)
+	}
+
+	err = s.cfg.SetUser(user.Name)
+	if err != nil {
+		return fmt.Errorf("couldn't set current user: %w", err)
+	}
+
+	fmt.Printf("%v registered successfully!\n", user.Name)
+	fmt.Println(user)
 	return nil
 }
--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"log"
+	"main/internal/config"
+	"main/internal/database"
 	"os"
 
-	"main/internal/config"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -17,14 +21,23 @@ func main() {
 		log.Fatalf("error reading config: %v", err)
 	}
 
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbQueries := database.New(db)
+
 	programState := &state{
 		cfg: &cfg,
+		db:  dbQueries,
 	}
 
 	cmds := commands{
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
