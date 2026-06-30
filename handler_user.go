45263d7495b74f76e38f120bd56eac5bd47c5738
RavenFayrin <raven.fayrin@gmail.com>
2026-06-30 16:27:52 -0400

    made to match lesson

--- handler_user.go
+++ handler_user.go
@@ -56,17 +56,17 @@ func handlerLogin(s *state, cmd command) error {
 	return nil
 }
 
-func handlerGetUsers(s *state, cmd command) error {
+func handlerListUsers(s *state, cmd command) error {
 	users, err := s.db.GetUsers(context.Background())
 	if err != nil {
-		return fmt.Errorf("couldn't find users: %w", err)
+		return fmt.Errorf("couldn't list users: %w", err)
 	}
 	for _, user := range users {
 		if user.Name == s.cfg.CurrentUserName {
-			fmt.Printf("* %s (current)\n", user.Name)
-		} else {
-			fmt.Printf("* %s\n", user.Name)
+			fmt.Printf("* %v (current)\n", user.Name)
+			continue
 		}
+		fmt.Printf("* %v\n", user.Name)
 	}
 	return nil
 }
