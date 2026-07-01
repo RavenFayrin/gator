194ed65353ce8f8b70bb5397cb4a0d3421a269b0
RavenFayrin <raven.fayrin@gmail.com>
2026-07-01 17:26:48 -0400

    made to match lesson

--- handler_feed.go
+++ handler_feed.go
@@ -11,11 +11,11 @@ import (
 func handlerAddFeed(s *state, cmd command) error {
 	user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
 	if err != nil {
-		return fmt.Errorf("couldn't find user: %w", err)
+		return err
 	}
 
 	if len(cmd.Args) != 2 {
-		return fmt.Errorf("usage: %v <name>", cmd.Name)
+		return fmt.Errorf("usage: %s <name> <url>", cmd.Name)
 	}
 
 	name := cmd.Args[0]
@@ -25,24 +25,27 @@ func handlerAddFeed(s *state, cmd command) error {
 		ID:        uuid.New(),
 		CreatedAt: time.Now().UTC(),
 		UpdatedAt: time.Now().UTC(),
+		UserID:    user.ID,
 		Name:      name,
 		Url:       url,
-		UserID:    user.ID,
 	})
 	if err != nil {
 		return fmt.Errorf("couldn't create feed: %w", err)
 	}
 
+	fmt.Println("Feed created successfully:")
 	printFeed(feed)
+	fmt.Println()
+	fmt.Println("=====================================")
 
 	return nil
 }
 
 func printFeed(feed database.Feed) {
-	fmt.Printf(" * ID:			%v\n", feed.ID)
-	fmt.Printf(" * Created At:	%v\n", feed.CreatedAt)
-	fmt.Printf(" * Updated At:	%v\n", feed.UpdatedAt)
-	fmt.Printf(" * Name:		%v\n", feed.Name)
-	fmt.Printf(" * Url:			%v\n", feed.Url)
-	fmt.Printf(" * UserID:		%v\n", feed.UserID)
+	fmt.Printf("* ID:            %s\n", feed.ID)
+	fmt.Printf("* Created:       %v\n", feed.CreatedAt)
+	fmt.Printf("* Updated:       %v\n", feed.UpdatedAt)
+	fmt.Printf("* Name:          %s\n", feed.Name)
+	fmt.Printf("* URL:           %s\n", feed.Url)
+	fmt.Printf("* UserID:        %s\n", feed.UserID)
 }
