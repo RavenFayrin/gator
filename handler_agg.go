042a8a3715924bca0870522217144f4c02bdc495
RavenFayrin <raven.fayrin@gmail.com>
2026-07-07 17:33:31 -0400

    updated agg function

--- handler_agg.go
+++ handler_agg.go
@@ -3,13 +3,48 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"time"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
-	if err != nil {
-		return fmt.Errorf("couldn't fetch feed: %w", err)
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <time>", cmd.Name)
 	}
-	fmt.Printf("Feed: %+v\n", feed)
-	return nil
+	time_between_reqs := cmd.Args[0]
+
+	timeBetweenRequests, err := time.ParseDuration(time_between_reqs)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Collecting Feeds every %v\n", timeBetweenRequests.String())
+	fmt.Println("------------------")
+
+	ticker := time.NewTicker(timeBetweenRequests)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *state) {
+	feed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = s.db.MarkFeedFetched(context.Background(), feed.ID)
+	if err != nil {
+		log.Println(err)
+	}
+
+	rss_feed, err := fetchFeed(context.Background(), feed.Url)
+	if err != nil {
+		log.Println(err)
+	}
+
+	for _, item := range rss_feed.Channel.Item {
+		fmt.Println(item.Title)
+		fmt.Println("")
+	}
 }
